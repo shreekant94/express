725878abac8d1e79a9bb30840f20f3d9e5c17818
shreekant <shreey94@gmail.com>
2024-10-25 23:34:35 +0530

    updated

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => {
-res.send({name:'shreekant'})
-})
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { config } = require("dotenv");
+app.use(express.json())
 
-app.listen(3000, () => { })
+
+const dburl = process.env.DB_URL
+
+
+app.use(cors());
+app.post("/login", (req, res) => {
+const { email, pass } = req.body;
+console.log(req.body);
+if (!email && !pass) {
+res.status(404).send("Please provide email and password" + req.body);
+} else {
+res.send("Login Success!" + req.body);
+}
+});
+// const auth = async (req,res,next)=>{
+// if(req.headers.authorization==="raj"){
+// next()
+// }else{
+// return res.send("invalid user")
+// }
+
+// }
+
+app.get("/getName", (req, res) => {
+
+res.send({
+name: "RAJ TILAK D SINGH ",
+});
+});
+
+app.post("/createUser", (req, res) => {
+const {email,password} = req.body
+if(!email || !password) {
+return res.status(400).send("please provide email and password")
+}else{
+res.status(200).send({
+msg: "User Created Succesfully",
+});
+}
+
+});
+
+app.listen(3500, () => {});
